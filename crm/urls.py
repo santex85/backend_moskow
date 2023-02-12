97be37ab505c1dcbba39c3b298148bad7efb495b
Alexander Cheremikin <santex85@gmail.com>
2023-02-12 17:53:50 +0700

    routing pages

--- crm/urls.py
+++ crm/urls.py
@@ -3,5 +3,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('booking-groups/', views.BookingGroups.as_view(), name='booking_groups'),
+    path('', views.IndexView.as_view(), name="index"),
+    path('report-period/', views.ReportPeriodView.as_view(), name="report-period"),
+    path('report-group/', views.ReportGroupView.as_view(), name="report-group"),
+    path('report-inline/', views.ReportInlineView.as_view(), name="report-inline"),
+    path('early-booking-group/', views.EarlyBookingGroupView.as_view(), name="early-booking-group"),
+    path('booking-group/', views.BookingGroupView.as_view(), name="booking-group"),
+    path('booking-guest/', views.BookingGuestView.as_view(), name="booking-guest"),
+    path('warehouse-accounting/', views.WarehouseAccountingView.as_view(), name="warehouse-accounting"),
+    path('kitchen-accounting/', views.KitchenAccountingView.as_view(), name="kitchen-accounting"),
+    path('calendar-hall/', views.CalendarHallView.as_view(), name="calendar-hall"),
+    path('calendar-sauna/', views.CalendarSaunaView.as_view(), name="calendar-sauna"),
+    path('cashier-income/', views.CashierIncomeView.as_view(), name="cashier-income"),
+    path('cashier-outcome/', views.CashierOutcomeView.as_view(), name="cashier-outcome"),
+    path('guests/', views.GuestsView.as_view(), name="guests"),
+    path('groups/', views.GroupsView.as_view(), name="groups"),
 ]
--- crm/views.py
+++ crm/views.py
@@ -4,14 +4,35 @@ from django.views import View
 from crm.forms import BookingGroupForm
 
 
-class BookingGroups(View):
+class IndexView(View):
+
+    def get(self, request):
+        return render(request, 'crm/index.html')
+
+
+class ReportPeriodView(View):
+    def get(self, request):
+        return render(request, 'crm/report_period.html')
+
+
+class ReportGroupView(View):
+    def get(self, request):
+        return render(request, 'crm/report_group.html')
+
+
+class ReportInlineView(View):
+    def get(self, request):
+        return render(request, 'crm/report_inline.html')
+
+
+class EarlyBookingGroupView(View):
     @staticmethod
     def get(request):
         form = BookingGroupForm()
         context = {
             'form': form,
         }
-        return render(request, 'crm/booking.html', context)
+        return render(request, 'crm/early_booking.html', context)
 
     @staticmethod
     def post(request):
@@ -23,4 +44,55 @@ class BookingGroups(View):
         context = {
             "form": form,
         }
-        return render(request, 'crm/booking.html', context)
+        return render(request, 'crm/early_booking.html', context)
+
+
+class BookingGroupView(View):
+    @staticmethod
+    def get(request):
+        return render(request, 'crm/booking_group.html')
+
+
+class BookingGuestView(View):
+    def get(self, request):
+        return render(request, 'crm/booking_guest.html')
+
+
+class WarehouseAccountingView(View):
+    def get(self, request):
+        return render(request, 'crm/warehouse_accounting.html')
+
+
+class KitchenAccountingView(View):
+    def get(self, request):
+        return render(request, 'crm/kitchen_accounting.html')
+
+
+class CalendarHallView(View):
+    def get(self, request):
+        return render(request, 'crm/calendar_hall.html')
+
+
+class CalendarSaunaView(View):
+    def get(self, request):
+        return render(request, 'crm/calendar_sauna.html')
+
+
+class CashierIncomeView(View):
+    def get(self, request):
+        return render(request, 'crm/cashier_income.html')
+
+
+class CashierOutcomeView(View):
+    def get(self, request):
+        return render(request, 'crm/cashier_outcome.html')
+
+
+class GuestsView(View):
+    def get(self, request):
+        return render(request, 'crm/guests.html')
+
+
+class GroupsView(View):
+    def get(self, request):
+        return render(request, 'crm/groups.html')
