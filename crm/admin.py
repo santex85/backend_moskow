ac13a64b467fa6c759764af90b08e9322572c6df
Alexander Cheremikin <santex85@gmail.com>
2023-02-14 08:45:12 +0700

    settings CRUD employer

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Employee, Position
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class EmployeesAdmin(UserAdmin):
+    model = Employee
+    list_display = ('username', 'first_name', 'last_name', 'position',)
+    fieldsets = (
+        (None, {"fields": ('username', 'password', 'email')}),
+        (None, {"fields": ('first_name', 'last_name', 'position')},),
+        (None, {"fields": ('is_superuser', 'is_staff', 'is_active', 'groups')}))
+
+
+admin.site.register(Employee, EmployeesAdmin)
+
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+admin.site.register(Position, PositionAdmin)
