8f1f137e83af0467966ca0ec3ea03d9eab9b7c10
Alexander Cheremikin <santex85@gmail.com>
2023-02-15 17:50:02 +0700

    add Objects(Hotels)View

--- crm/views.py
+++ crm/views.py
@@ -1,13 +1,36 @@
+import random
+
+from django.db.models import Sum
 from django.shortcuts import render, redirect
 from django.views import View
+from faker import Faker
 
 from crm.forms import BookingGroupForm
-from crm.models import Group
+from crm.helpers import change_fullness, get_title_object_info
+from crm.models import Group, Hotel, Room
 
 
 class IndexView(View):
 
     def get(self, request):
+        # todo create main page q: what is the main page?
+        # fake = Faker()
+        #
+        # # Создаем 5 отелей
+        # hotels = [Hotel.objects.create(name=fake.company()) for _ in
+        #           range(5)]
+        #
+        # # Создаем 50 номеров (10 номеров на каждый отель)
+        # for hotel in hotels:
+        #     for i in range(10):
+        #         name = fake.word() + " Room"
+        #         capacity = random.choice([2, 3, 4])
+        #         price = random.randint(50, 200)
+        #         fullness = random.choice(["full", "partially", "empty"])
+        #         number_guests = random.randint(0, capacity)
+        #         over_booking = random.randint(0, 2)
+        #         Room.objects.create(name=name, hotel=hotel, capacity=capacity, price=price, fullness=fullness,
+        #                             number_guests=number_guests, over_booking=over_booking)
         return render(request, 'crm/index.html')
 
 
@@ -94,6 +117,15 @@ class GuestsView(View):
         return render(request, 'crm/guests.html')
 
 
+class GuestView(View):
+    def get(self, request, pk):
+        guest = Hotel.objects.get(pk=pk)
+        context = {
+            'guest': guest,
+        }
+        return render(request, 'crm/guest.html', context)
+
+
 class GroupsView(View):
     @staticmethod
     def get(request):
@@ -111,3 +143,28 @@ class GroupView(View):
             'group': group,
         }
         return render(request, 'crm/group.html', context)
+
+
+class ObjectsView(View):
+    @staticmethod
+    def get(request):
+        objects = Hotel.objects.all()
+        # todo
+        for obj in objects:
+            get_title_object_info(obj)
+        context = {
+            'objects': objects,
+        }
+        print(context)
+        return render(request, 'crm/objects.html', context)
+
+
+class ObjectView(View):
+    @staticmethod
+    def get(request, pk):
+        # todo get data for page with object
+        hotel = Hotel.objects.get(pk=pk)
+        context = {
+            'hotel': hotel,
+        }
+        return render(request, 'crm/object.html', context)
