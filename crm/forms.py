f22e676f66a3ba37bd709d2088963a786160a11d
Alexander Cheremikin <santex85@gmail.com>
2023-02-16 16:00:13 +0700

    add Kitchen form

--- crm/forms.py
+++ crm/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from crm.models import Group
+from crm.models import Group, Groceries
 
 
 class BookingGroupForm(forms.ModelForm):
@@ -8,6 +8,12 @@ class BookingGroupForm(forms.ModelForm):
         model = Group
         fields = "__all__"
         widgets = {
-            'date_checkin': forms.DateInput(attrs={'type': 'date'}),
-            'date_checkout': forms.DateInput(attrs={'type': 'date'}),
+            "date_checkin": forms.DateInput(attrs={"type": "date"}),
+            "date_checkout": forms.DateInput(attrs={"type": "date"}),
         }
+
+
+class KitchenForm(forms.ModelForm):
+    class Meta:
+        model = Groceries
+        fields = "__all__"
