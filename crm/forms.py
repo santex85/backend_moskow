0e07dd564bf210fe468f35f465705da7b82a86c2
Alexander Cheremikin <santex85@gmail.com>
2023-02-19 16:17:54 +0700

    change field in db

--- crm/forms.py
+++ crm/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from crm.models import Group, Groceries
+from crm.models import Group, Groceries, Household
 
 
 class BookingGroupForm(forms.ModelForm):
@@ -17,3 +17,21 @@ class KitchenForm(forms.ModelForm):
     class Meta:
         model = Groceries
         fields = "__all__"
+
+
+class KitchenUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Groceries
+        fields = ["how_many_unit", "price"]
+
+
+class HouseholdForm(forms.ModelForm):
+    class Meta:
+        model = Household
+        fields = "__all__"
+
+
+class HouseholdUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Groceries
+        fields = ["how_many_unit", "price"]
